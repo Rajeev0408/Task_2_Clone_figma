a6e3fb02a5491582a7038e15b2b1f59cd382e0c4
Rajeev0408 <rajeevcarpenter04@gmail.com>
2024-09-04 22:08:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { BrowserRouter, Route, Routes, Link, Navigate } from 'react-router-dom';
+import Home from './compomnents/Home';
+import About from './compomnents/About';
+import { Footer } from './compomnents/footer/Footer';
+import Header from './compomnents/header/Header';
+import FindJob from './compomnents/FindJob';
+import FindWarker from './compomnents/FindWarker';
+import Resources from './compomnents/Resources';
+import Login from './compomnents/Login';
+
+
+function App() {
+  return (
+    <>
+       <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/findJobs" element={<FindJob />} />
+          <Route path="/findWorker" element={<FindWarker />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
